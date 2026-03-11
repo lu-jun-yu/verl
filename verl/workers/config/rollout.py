baa781a1bfdc7fa0dc756dbc8eee769c0e485a48
lu-jun-yu <jun_yu_lu@163.com>
2026-03-11 16:27:31 +0800

    更新OPTS_TTPO参数

--- verl/workers/config/rollout.py
+++ verl/workers/config/rollout.py
@@ -128,8 +128,8 @@ class RolloutConfig(BaseConfig):
     top_p: float = 1.0
     do_sample: bool = True
     n: int = 1
-    g: int = 1
-    root_tuct: float = 0.0
+    max_search_per_tree: int = 4
+    c: float = 1.0
     search: str = None
     repetition_penalty: float = 1.0
 

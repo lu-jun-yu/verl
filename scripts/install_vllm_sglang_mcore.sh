4ae2b3e8ef2291473fb566d4f450ecaa8cc05d55
lu-jun-yu <jun_yu_lu@163.com>
2026-03-19 14:52:08 +0800

    fix verl install bug

--- scripts/install_vllm_sglang_mcore.sh
+++ scripts/install_vllm_sglang_mcore.sh
@@ -14,14 +14,14 @@ pip install --no-cache-dir "vllm==0.11.0"
 echo "2. install basic packages"
 pip install "transformers[hf_xet]>=4.51.0" accelerate datasets peft hf-transfer \
     "numpy<2.0.0" "pyarrow>=15.0.0" pandas "tensordict>=0.8.0,<=0.10.0,!=0.9.0" torchdata \
-    ray[default] codetiming hydra-core pylatexenc qwen-vl-utils wandb dill pybind11 liger-kernel mathruler \
+    "ray[default]==2.48.0" codetiming hydra-core pylatexenc qwen-vl-utils wandb dill pybind11 liger-kernel mathruler \
     pytest py-spy pre-commit ruff tensorboard 
 
 echo "pyext is lack of maintainace and cannot work with python 3.12."
 echo "if you need it for prime code rewarding, please install using patched fork:"
 echo "pip install git+https://github.com/ShaohonChen/PyExt.git@py311support"
 
-pip install "nvidia-ml-py>=12.560.30" "fastapi[standard]>=0.115.0" "optree>=0.13.0" "pydantic>=2.9" "grpcio>=1.62.1"
+pip install "nvidia-ml-py>=12.560.30" "fastapi[standard]>=0.115.0" "optree>=0.13.0" "pydantic>=2.9" "grpcio==1.62.1"
 
 
 echo "3. install FlashAttention and FlashInfer"
